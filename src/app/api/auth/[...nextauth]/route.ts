6e262389e6d5f3a8f70f2df1aa99ed1128a60314
ivooo369 <ivoivelinov@abv.bg>
2024-10-25 12:53:22 +0300

    Integrate Prisma ORM and implement admin authentication and authorization for dashboard access

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const username = credentials?.username as string;
-        const password = credentials?.password as string;
-
-        try {
-          const user = await prisma.admin.findUnique({
-            where: { username },
-          });
-
-          if (user && (await verifyPassword(password, user.password))) {
-            return user;
-          }
-
-          return null;
-        } catch (error) {
-          console.error("Authorization error:", error);
-          return null;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/admin/login",
-  },
-  session: {
-    maxAge: 60 * 15,
-    updateAge: 60 * 5,
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
-async function verifyPassword(plainPassword: string, hashedPassword: string) {
-  return await bcrypt.compare(plainPassword, hashedPassword);
-}
+import { authOptions } from "@/app/lib/options";
 
 const handler = NextAuth(authOptions);
 
