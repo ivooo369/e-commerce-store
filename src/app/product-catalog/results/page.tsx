2e3af41eb213309da001d4765f8faad4d4126fb9
ivooo369 <ivoivelinov@abv.bg>
2024-12-15 01:18:58 +0200

    Fix a production bug

--- src/app/product-catalog/results/page.tsx
+++ src/app/product-catalog/results/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { Product } from "@prisma/client";
 import ProductCard from "@/app/ui/components/product-card";
@@ -60,43 +60,12 @@ export default function ResultsPage() {
   };
 
   return (
-    <div className="products-filter-container flex flex-col items-center max-w-screen-2xl mx-auto p-8">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        Резултати за &quot;{query}&quot;
-      </h1>
-      {filteredProducts.length > 0 && (
-        <div>
-          <PaginationButtons
-            itemsPerPage={ITEMS_PER_PAGE}
-            totalItems={filteredProducts.length}
-            paginate={paginate}
-            currentPage={currentPage}
-          />
-        </div>
-      )}
-      {loading ? (
-        <Box className="flex justify-center items-center py-10 my-auto">
-          <CircularProgress message="Зареждане на продуктите..." />
-        </Box>
-      ) : currentItems.length === 0 ? (
-        <div className="container mx-auto px-4 font-bold">
-          <p className="text-center text-2xl p-16 bg-white rounded-md text-gray-600">
-            Няма намерени продукти
-          </p>
-        </div>
-      ) : (
-        <div>
-          <div className="container my-7 py-4 lg:px-4">
-            <div className="grid gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {currentItems.map((product) => (
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  onAddToCart={handleAddToCart}
-                />
-              ))}
-            </div>
-          </div>
+    <Suspense fallback={<div>Зареждане...</div>}>
+      <div className="products-filter-container flex flex-col items-center max-w-screen-2xl mx-auto p-8">
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Резултати за &quot;{query}&quot;
+        </h1>
+        {filteredProducts.length > 0 && (
           <div>
             <PaginationButtons
               itemsPerPage={ITEMS_PER_PAGE}
@@ -105,8 +74,41 @@ export default function ResultsPage() {
               currentPage={currentPage}
             />
           </div>
-        </div>
-      )}
-    </div>
+        )}
+        {loading ? (
+          <Box className="flex justify-center items-center py-10 my-auto">
+            <CircularProgress message="Зареждане на продуктите..." />
+          </Box>
+        ) : currentItems.length === 0 ? (
+          <div className="container mx-auto px-4 font-bold">
+            <p className="text-center text-2xl p-16 bg-white rounded-md text-gray-600">
+              Няма намерени продукти
+            </p>
+          </div>
+        ) : (
+          <div>
+            <div className="container my-7 py-4 lg:px-4">
+              <div className="grid gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {currentItems.map((product) => (
+                  <ProductCard
+                    key={product.id}
+                    product={product}
+                    onAddToCart={handleAddToCart}
+                  />
+                ))}
+              </div>
+            </div>
+            <div>
+              <PaginationButtons
+                itemsPerPage={ITEMS_PER_PAGE}
+                totalItems={filteredProducts.length}
+                paginate={paginate}
+                currentPage={currentPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </Suspense>
   );
 }
